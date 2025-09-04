e7cb0b06cb0c0334f8ba97b84a11a51a9dc7d71f
divyamsharma184 <95537884+divyamsharma184@users.noreply.github.com>
2025-09-04 22:15:46 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/version/cpp/189-RotateArray_20250904_221545.cpp
@@ -0,0 +1,21 @@
+// Last updated: 9/4/2025, 10:15:45 PM
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;  
+        vector<int> temp(k);
+        for(int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+        for(int i = n - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
+        }
+        for(int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+};
