cab6da3ed9337b7b28e87554b904014194385fb6
divyamsharma184 <95537884+divyamsharma184@users.noreply.github.com>
2025-09-21 04:18:02 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/version/cpp/234-PalindromeLinkedList_20250921_041800.cpp
@@ -0,0 +1,59 @@
+// Last updated: 9/21/2025, 4:18:00 AM
+//**
+ //* Definition for singly-linked list.
+ //* struct ListNode {
+ //*     int val;
+ //*     ListNode *next;
+ //*     ListNode() : val(0), next(nullptr) {}
+ //*     ListNode(int x) : val(x), next(nullptr) {}
+ //*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ //* };
+ //
+class Solution {
+public:
+  
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        
+        ListNode* newHead = reverse(slow);
+        ListNode* first = head;
+        ListNode* second = newHead;
+
+        
+        bool result = true;
+        while (second != nullptr) {
+            if (first->val != second->val) {
+                result = false;
+                break;
+            }
+            first = first->next;
+            second = second->next;
+        }
+
+        
+        reverse(newHead);
+
+        return result;
+    }
+};
