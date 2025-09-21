271d393d8f6f19caf1bb930764d9f0721cde1274
divyamsharma184 <95537884+divyamsharma184@users.noreply.github.com>
2025-09-21 16:58:08 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/version/cpp/217-ContainsDuplicate_20250921_165807.cpp
@@ -0,0 +1,13 @@
+// Last updated: 9/21/2025, 4:58:07 PM
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for(int num : nums) {
+            if(seen.count(num)) return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
+
