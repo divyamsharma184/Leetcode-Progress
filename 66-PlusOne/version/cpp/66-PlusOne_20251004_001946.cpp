f5acc1bde37057f22e3974a23092d48aad07ef69
divyamsharma184 <95537884+divyamsharma184@users.noreply.github.com>
2025-10-04 00:19:47 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/version/cpp/66-PlusOne_20251004_001946.cpp
@@ -0,0 +1,20 @@
+// Last updated: 10/4/2025, 12:19:46 AM
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        for(int i = n - 1; i >= 0; i--) {
+            if(digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                return digits; 
+            }
+        }
+
+        
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
